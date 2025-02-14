09cb73f507010f0f0664481d4624288434bd92c9
KamKava <kk2085@hw.ac.uk>
2025-02-14 13:32:02 +0000

    Update dashboard.php

--- protohome/html/dashboard.php
+++ protohome/html/dashboard.php
@@ -19,7 +19,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="dashboard.html">Dashboard</a>
+                        <a class="nav-link active" aria-current="page" href="dashboard.php">Dashboard</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="energyreport.html">Energy Report</a>
