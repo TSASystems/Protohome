404416576762b59d39f85358351ea8d457ea057c
ma2178 <ma2178@hw.ac.uk>
2025-03-03 17:03:26 +0000

    device name functionality added to dashboard

--- protohome/js/dashboard.js
+++ protohome/js/dashboard.js
@@ -8,19 +8,31 @@ function addBox() {
     grid.insertBefore(box, grid.lastElementChild);
 }
 
+function closeInterface() {
+	document.getElementById("addDevice").style.display = "none";
+}
+
+function showInterface() {
+	document.getElementById("addDevice").style.display = "block";
+	document.getElementById("name").value = "";
+}
+
 //concept with lamp
 function addDevice() {
+	//get value from text input
+	var name = document.getElementById("name").value;
+	
+	closeInterface();
     const grid = document.getElementById("grid");
     const box = document.createElement("div");
     box.classList.add("device");
-    box.innerHTML = "<p>Lamp</p><img src = '../image/Lamp.PNG' alt = 'lamp' class = 'devImg'>";
+	
+	//concatenate html for box
+	let html = "<p>";
+	html += name;
+	html += "</p><img src = '../image/Lamp.PNG' alt = 'lamp' class = 'devImg'>";
+	
+    box.innerHTML = html;
     box.onclick = function() { grid.removeChild(box); };
     grid.insertBefore(box, grid.lastElementChild);
-	
-	//display add device interface
-	document.getElementById("addDevice").style.display = "block";
-}
-
-function closeInterface() {
-	document.getElementById("addDevice").style.display = "none";
 }
