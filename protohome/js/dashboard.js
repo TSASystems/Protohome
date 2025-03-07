0bea0f717d2465ddec0f2dbee191e783570b3146
Shjhshi <zy3002@hw.ac.uk>
2025-03-07 13:48:38 +0000

    Add all divcie in the dashboard

--- protohome/js/dashboard.js
+++ protohome/js/dashboard.js
@@ -40,10 +40,25 @@ function displayAvailableDevices(id, text, target) {
   }
 
   
-  displayAvailableDevices('list-lamp-list', 'Lamp', '#list-lamp');
-  displayAvailableDevices('list-centralheating-list', 'Central Heating', '#list-centralheating');
-  displayAvailableDevices('list-dishwasher-list', 'Dishwasher', '#list-dishwasher');
-  displayAvailableDevices('list-wifirouter-list', 'Wi-Fi Router', '#list-wifirouter');
+    displayAvailableDevices('list-lamp-list', 'Lamp', '#list-lamp');
+    displayAvailableDevices('list-centralheating-list', 'Central Heating', '#list-centralheating');
+    displayAvailableDevices('list-dishwasher-list', 'Dishwasher', '#list-dishwasher');
+    displayAvailableDevices('list-wifirouter-list', 'Wi-Fi Router', '#list-wifirouter');
+    displayAvailableDevices('list-blender-list', 'Blender', '#list-blender');
+    displayAvailableDevices('list-iron-list', 'Iron', '#list-iron');
+    displayAvailableDevices('list-microwave-list', 'Microwave', '#list-microwave');
+    displayAvailableDevices('list-washingmachine-list', 'Washing Machine', '#list-washingmachine');
+    displayAvailableDevices('list-hairdryer-list', 'Hair Dryer', '#list-hairdryer');
+    displayAvailableDevices('list-airconditioner-list', 'Air Conditioner', '#list-airconditioner');
+    displayAvailableDevices('list-tv-list', 'TV', '#list-tv');
+    displayAvailableDevices('list-laptop-list', 'Laptop', '#list-laptop');
+    displayAvailableDevices('list-gameingconsole-list', 'Gameing Console', '#list-gameingconsole');
+    displayAvailableDevices('list-toaster-list', 'Toaster', '#list-toaster');
+    displayAvailableDevices('list-coffeemaker-list', 'Coffee Maker', '#list-coffeemaker');
+    displayAvailableDevices('list-tablet-list', 'Tablet', '#list-tablet');
+    displayAvailableDevices('list-waterheater-list', 'Water Heater', '#list-waterheater');
+    displayAvailableDevices('list-electricheater-list', 'Electric Heater', '#list-electricheater');
+    displayAvailableDevices('list-charger-list', 'Charger', '#list-charger');
 
 
 
