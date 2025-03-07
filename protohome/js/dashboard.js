274115ed0c192aba8b23b34f26c80f63c51b39dd
ma2178 <ma2178@hw.ac.uk>
2025-03-07 20:48:46 +0000

    Dashboard add device functionality images fixed

--- protohome/js/dashboard.js
+++ protohome/js/dashboard.js
@@ -25,7 +25,7 @@ function showInterface() {
 
 }
 
-function displayAvailableDevices(id, address, text, target) {
+function displayAvailableDevices(id, text, target) {
   
     const button = document.createElement('button');
     button.className = 'list-group-item list-group-item-action';
@@ -37,7 +37,7 @@ function displayAvailableDevices(id, address, text, target) {
     button.setAttribute('aria-controls', target.substring(1));
     const img=document.createElement('img');
     
-    img.src = '../image/'+address+'.PNG';
+    img.src = '../image/'+text+'.PNG';
     img.width = 40;
     img.height = 40;
     const ButtonText = document.createTextNode(text);
@@ -50,25 +50,25 @@ function displayAvailableDevices(id, address, text, target) {
     document.getElementById('list-tab').appendChild(button);
   }
 
-  displayAvailableDevices('list-airconditioner-list','AirConditioner', 'Air Conditioner', '#list-airconditioner');
-  displayAvailableDevices('list-blender-list','Blender', 'Blender', '#list-blender');
-  displayAvailableDevices('list-centralheating-list','CentralHeating', 'Central Heating', '#list-centralheating');
-  displayAvailableDevices('list-coffeemaker-list','CoffeeMaker', 'Coffee Maker', '#list-coffeemaker');
-  displayAvailableDevices('list-charger-list','Charger', 'Charger', '#list-charger');
-  displayAvailableDevices('list-dishwasher-list','Dishwasher', 'Dishwasher', '#list-dishwasher');
-  displayAvailableDevices('list-gameingconsole-list','GameConsole' ,'Gaming Console', '#list-gameingconsole');
-  displayAvailableDevices('list-hairdryer-list','HairDryer', 'Hair Dryer', '#list-hairdryer');
-  displayAvailableDevices('list-iron-list','Iron' ,'Iron', '#list-iron');
-  displayAvailableDevices('list-lamp-list','Lamp', 'Lamp', '#list-lamp');
-  displayAvailableDevices('list-laptop-list','Laptop', 'Laptop', '#list-laptop');
-  displayAvailableDevices('list-microwave-list','Microwave', 'Microwave', '#list-microwave');
-  displayAvailableDevices('list-tv-list','TV', 'TV', '#list-tv');
-  displayAvailableDevices('list-tablet-list','Desktop', 'Desktop', '#list-tablet');
-  displayAvailableDevices('list-toaster-list','Toaster', 'Toaster', '#list-toaster')
-  displayAvailableDevices('list-vacuumcleaner-list','VacuumCleaner', 'Vacuum Cleaner', '#list-vacuumcleaner');
-  displayAvailableDevices('list-waterheater-list','WaterHeater', 'Water Heater', '#list-waterheater');
-  displayAvailableDevices('list-washingmachine-list','WashingMachine', 'Washing Machine', '#list-washingmachine');  
-  displayAvailableDevices('list-wifirouter-list','WifiRouter', 'Wi-Fi Router', '#list-wifirouter');
+  displayAvailableDevices('list-airconditioner-list', 'Air Conditioner', '#list-airconditioner');
+  displayAvailableDevices('list-blender-list', 'Blender', '#list-blender');
+  displayAvailableDevices('list-centralheating-list', 'Central Heating', '#list-centralheating');
+  displayAvailableDevices('list-coffeemaker-list', 'Coffee Maker', '#list-coffeemaker');
+  displayAvailableDevices('list-charger-list', 'Charger', '#list-charger');
+  displayAvailableDevices('list-dishwasher-list', 'Dishwasher', '#list-dishwasher');
+  displayAvailableDevices('list-gameconsole-list', 'Game Console', '#list-gameconsole');
+  displayAvailableDevices('list-hairdryer-list', 'Hair Dryer', '#list-hairdryer');
+  displayAvailableDevices('list-iron-list', 'Iron', '#list-iron');
+  displayAvailableDevices('list-lamp-list', 'Lamp', '#list-lamp');
+  displayAvailableDevices('list-laptop-list', 'Laptop', '#list-laptop');
+  displayAvailableDevices('list-microwave-list', 'Microwave', '#list-microwave');
+  displayAvailableDevices('list-tv-list', 'TV', '#list-tv');
+  displayAvailableDevices('list-tablet-list', 'Desktop', '#list-tablet');
+  displayAvailableDevices('list-toaster-list', 'Toaster', '#list-toaster')
+  displayAvailableDevices('list-vacuumcleaner-list', 'Vacuum Cleaner', '#list-vacuumcleaner');
+  displayAvailableDevices('list-waterheater-list', 'Water Heater', '#list-waterheater');
+  displayAvailableDevices('list-washingmachine-list', 'Washing Machine', '#list-washingmachine');  
+  displayAvailableDevices('list-wifirouter-list', 'Wi-Fi Router', '#list-wifirouter');
     
     
     
@@ -94,7 +94,7 @@ function displayAvailableDevices(id, address, text, target) {
 function addDevice() {
 	//get highlighted button
 	var toAdd = document.getElementsByClassName("list-group-item list-group-item-action active");
-	var name = toAdd[0].innerText;
+	var name = toAdd[0].innerText.trim();
 	
     const grid = document.getElementById("grid");
     const box = document.createElement("div");
@@ -103,7 +103,11 @@ function addDevice() {
 	//concatenate html for box
 	let html = "<p>";
 	html += name;
-	html += "</p><img src = '../image/Lamp.PNG' alt = 'lamp' class = 'devImg'>";
+	html += "</p><img src = '../image/"
+	html += name;
+	html += ".PNG' class = 'devImg' alt='";
+	html += name;
+	html += "'>";
 	
     box.innerHTML = html;
     box.onclick = function() { grid.removeChild(box); };
