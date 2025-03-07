a47ad5b4bb9ce4d03b48bb37125175081788f3f3
Shjhshi <zy3002@hw.ac.uk>
2025-03-07 17:39:41 +0000

    Add the icons to the dashboard when adding  divice and sizes of some of them have been fixed. fix of the picture in the index to cater the style of the website

--- protohome/js/dashboard.js
+++ protohome/js/dashboard.js
@@ -25,40 +25,64 @@ function showInterface() {
 
 }
 
-function displayAvailableDevices(id, text, target) {
+function displayAvailableDevices(id, address, text, target) {
   
     const button = document.createElement('button');
     button.className = 'list-group-item list-group-item-action';
     button.id = id;
+    
     button.setAttribute('data-bs-toggle', 'list');
     button.setAttribute('href', target);
     button.setAttribute('role', 'tab');
-    button.setAttribute('aria-controls', target.substring(1)); 
-    button.textContent = text;
-
+    button.setAttribute('aria-controls', target.substring(1));
+    const img=document.createElement('img');
+    
+    img.src = '../image/'+address+'.PNG';
+    img.width = 40;
+    img.height = 40;
+    const ButtonText = document.createTextNode(text);
+    const Space = document.createTextNode(' ');
+    
+    
+    button.appendChild(img);
+    button.appendChild(Space);
+    button.appendChild(ButtonText);// button.textContent = text;
     document.getElementById('list-tab').appendChild(button);
   }
 
-  
-    displayAvailableDevices('list-lamp-list', 'Lamp', '#list-lamp');
-    displayAvailableDevices('list-centralheating-list', 'Central Heating', '#list-centralheating');
-    displayAvailableDevices('list-dishwasher-list', 'Dishwasher', '#list-dishwasher');
-    displayAvailableDevices('list-wifirouter-list', 'Wi-Fi Router', '#list-wifirouter');
-    displayAvailableDevices('list-blender-list', 'Blender', '#list-blender');
-    displayAvailableDevices('list-iron-list', 'Iron', '#list-iron');
-    displayAvailableDevices('list-microwave-list', 'Microwave', '#list-microwave');
-    displayAvailableDevices('list-washingmachine-list', 'Washing Machine', '#list-washingmachine');
-    displayAvailableDevices('list-hairdryer-list', 'Hair Dryer', '#list-hairdryer');
-    displayAvailableDevices('list-airconditioner-list', 'Air Conditioner', '#list-airconditioner');
-    displayAvailableDevices('list-tv-list', 'TV', '#list-tv');
-    displayAvailableDevices('list-laptop-list', 'Laptop', '#list-laptop');
-    displayAvailableDevices('list-gameingconsole-list', 'Gameing Console', '#list-gameingconsole');
-    displayAvailableDevices('list-toaster-list', 'Toaster', '#list-toaster');
-    displayAvailableDevices('list-coffeemaker-list', 'Coffee Maker', '#list-coffeemaker');
-    displayAvailableDevices('list-tablet-list', 'Tablet', '#list-tablet');
-    displayAvailableDevices('list-waterheater-list', 'Water Heater', '#list-waterheater');
-    displayAvailableDevices('list-electricheater-list', 'Electric Heater', '#list-electricheater');
-    displayAvailableDevices('list-charger-list', 'Charger', '#list-charger');
+  displayAvailableDevices('list-airconditioner-list','AirConditioner', 'Air Conditioner', '#list-airconditioner');
+  displayAvailableDevices('list-blender-list','Blender', 'Blender', '#list-blender');
+  displayAvailableDevices('list-centralheating-list','CentralHeating', 'Central Heating', '#list-centralheating');
+  displayAvailableDevices('list-coffeemaker-list','CoffeeMaker', 'Coffee Maker', '#list-coffeemaker');
+  displayAvailableDevices('list-charger-list','Charger', 'Charger', '#list-charger');
+  displayAvailableDevices('list-dishwasher-list','Dishwasher', 'Dishwasher', '#list-dishwasher');
+  displayAvailableDevices('list-gameingconsole-list','GameConsole' ,'Gaming Console', '#list-gameingconsole');
+  displayAvailableDevices('list-hairdryer-list','HairDryer', 'Hair Dryer', '#list-hairdryer');
+  displayAvailableDevices('list-iron-list','Iron' ,'Iron', '#list-iron');
+  displayAvailableDevices('list-lamp-list','Lamp', 'Lamp', '#list-lamp');
+  displayAvailableDevices('list-laptop-list','Laptop', 'Laptop', '#list-laptop');
+  displayAvailableDevices('list-microwave-list','Microwave', 'Microwave', '#list-microwave');
+  displayAvailableDevices('list-tv-list','TV', 'TV', '#list-tv');
+  displayAvailableDevices('list-tablet-list','Desktop', 'Desktop', '#list-tablet');
+  displayAvailableDevices('list-toaster-list','Toaster', 'Toaster', '#list-toaster')
+  displayAvailableDevices('list-vacuumcleaner-list','VacuumCleaner', 'Vacuum Cleaner', '#list-vacuumcleaner');
+  displayAvailableDevices('list-waterheater-list','WaterHeater', 'Water Heater', '#list-waterheater');
+  displayAvailableDevices('list-washingmachine-list','WashingMachine', 'Washing Machine', '#list-washingmachine');  
+  displayAvailableDevices('list-wifirouter-list','WifiRouter', 'Wi-Fi Router', '#list-wifirouter');
+    
+    
+    
+   
+    
+
+    
+ 
+    
+ 
+    
+    
+    
+    
 
 
 
